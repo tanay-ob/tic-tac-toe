c40cbe764f58eb21b4d40b5df0071046a9d6e941
Tanay <shahtanay17899@gmail.com>
2024-09-14 15:06:52 +0530

    Completed Part 2

--- src/Board.js
+++ src/Board.js
@@ -1,19 +1,41 @@
+import Confetti from "react-confetti";
 import Box from "./Box";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function Board() {
   const [boxes, setBoxes] = useState(Array(9).fill(""));
   const [curSign, setCurSign] = useState("X");
+  const [winner, setWinner] = useState("");
+  const [isMatchDraw, setIsMatchDraw] = useState(false);
 
   const checkForWinner = () => {
-    if (boxes[0] !== "" && boxes[0] === boxes[1] && boxes[1] === boxes[2]) {
-      alert("We got winner: " + boxes[0]);
+    const winnerPos = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let i = 0; i < winnerPos.length; i++) {
+      let [pos1, pos2, pos3] = winnerPos[i]; // [0, 4, 8] - > 0,1,2
+      if (
+        boxes[pos1] !== "" &&
+        boxes[pos1] === boxes[pos2] &&
+        boxes[pos2] === boxes[pos3]
+      ) {
+        setWinner(boxes[pos1]);
+        break;
+      }
     }
   };
 
   const addSign = (idx) => {
     // if box is filled, no need to do anything
-    if (boxes[idx] !== "") {
+    if (boxes[idx] !== "" || winner !== "") {
       return;
     }
 
@@ -22,18 +44,41 @@ function Board() {
     newboxes[idx] = curSign;
     setBoxes(newboxes);
 
-    checkForWinner();
-
     // update the sign
     setCurSign(curSign === "O" ? "X" : "O");
   };
 
+  const checkForDraw = () => {
+    const allChecked = boxes.filter((box) => box === "").length === 0;
+    if (winner === "" && allChecked) {
+      setIsMatchDraw(true);
+    }
+  };
+
+  useEffect(() => {
+    checkForWinner();
+    checkForDraw();
+  }, [boxes]);
+
+  const restartGame = () => {
+    setBoxes(Array(9).fill(""));
+    setWinner("");
+    setCurSign("");
+    setIsMatchDraw(false);
+  };
+
   return (
-    <div className="board">
-      {boxes.map((boxValue, idx) => (
-        <Box sign={boxValue} addSign={() => addSign(idx)} />
-      ))}
-    </div>
+    <>
+      {winner !== "" && <Confetti />}
+      {winner !== "" && <h1>Winner is {winner}!!</h1>}
+      {isMatchDraw && <h1>Match Draw!!</h1>}
+      <div className="board">
+        {boxes.map((boxValue, idx) => (
+          <Box sign={boxValue} addSign={() => addSign(idx)} />
+        ))}
+      </div>
+      <button onClick={restartGame}>Restart</button>
+    </>
   );
 }
 
